87faad5da3a75a4693e87f3eaa44c1e216d90689
Andrei-Khadziukou <Andrei_Khadziukou@epam.com>
2017-04-19 13:46:09 +0300

    Service discovery was enabled for Config Server. Configuration of retrieving properties from GitHub repository was adjusted

--- src/main/java/com/epam/petclinic/config/PetClinicConfigApplication.java
+++ src/main/java/com/epam/petclinic/config/PetClinicConfigApplication.java
@@ -2,10 +2,14 @@ package com.epam.petclinic.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class PetClinicConfigApplication {
 
     public static void main(String[] args) {
